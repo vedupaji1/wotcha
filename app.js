f3acc6dbb583d71bbc45173bd69e2828e7eddb03
vedupaji1 <vedupaji@gmail.com>
2021-06-26 14:51:23 +0530

    nodejs app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ||5000;
+app.use(express.static("public"));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+app.listen(PORT, () => {
+    console.log(`Ok`);
+})
