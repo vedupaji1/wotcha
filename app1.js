64a0322cb30b822d85a3bb6cc37783cbe4423057
vedupaji1 <vedupaji@gmail.com>
2021-06-26 15:27:58 +0530

    wotchaOP

--- app1.js
+++ app1.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT ||5000;
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render("index")
 })
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`Ok`);
 })
